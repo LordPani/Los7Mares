03e3a5089d8116ff72103e93e54b0cf063e697a3
Saylarx <meginopovedalex@gmail.com>
2024-04-16 09:04:20 +0200

    Si o no

--- proyectoPirata/src/agentes/Recluta.java
+++ proyectoPirata/src/agentes/Recluta.java
@@ -2,15 +2,16 @@ package agentes;
 
 /**
  * Esta clase es la de los soldados rasos de la Marina, los reclutas.
+ * 
  * @author Sergio
  * @version 1.2
  */
 
 public class Recluta extends Justiciero {
-	
+
 	private String colorGorro;
 	private int anclasEchadas;
-	
+
 	public Recluta(String nombre, int edad, int anyosExperiencia, int punteria, int numMedallasHonor, String gorro,
 			int anclasEchadas) {
 		super(nombre, edad, anyosExperiencia, punteria, numMedallasHonor);
@@ -33,42 +34,43 @@ public class Recluta extends Justiciero {
 	public void setAnclasEchadas(int anclasEchadas) {
 		this.anclasEchadas = anclasEchadas;
 	}
-	
+
 	/**
-	 * Metodo que sirve para que el recluta eche el ancla. Si supera 51 anclas echadas, se gana una medalla de honor.
+	 * Metodo que sirve para que el recluta eche el ancla. Si supera 51 anclas
+	 * echadas, se gana una medalla de honor.
+	 * 
 	 * @return Devuelve el total de anclas echadas.
 	 * @since 1.0
 	 * @see Justiciero#numMedallasHonor
 	 */
-	
+
 	public int echarAncla() {
-		System.out.println("¡Voy a echar el ancla! ¡Vamos a frenar!");
+		System.out.println("¡Voy a echar el ancla! ¡Vamos a frenar! Niki nicole si o no Diego??");
 		anclasEchadas++;
-		if(this.anclasEchadas > 51) {
+		if (this.anclasEchadas > 51) {
 			numMedallasHonor++;
 		}
 		return this.anclasEchadas;
 	}
-	
+
 	/**
-	 * Metodo que sirve para que el recluta se ponga su gorro. Dependiendo del color, aumenta su punteria mas o menos.
+	 * Metodo que sirve para que el recluta se ponga su gorro. Dependiendo del
+	 * color, aumenta su punteria mas o menos.
+	 * 
 	 * @since 1.0
 	 * @see #colorGorro
 	 */
-	
+
 	public void ponerseGorro() {
 		System.out.println("Me voy a poner mi gorro de color " + colorGorro);
-		if(colorGorro.equalsIgnoreCase("verde")) {
+		if (colorGorro.equalsIgnoreCase("verde")) {
 			this.punteria += 7;
-		}
-		else if(colorGorro.equalsIgnoreCase("morado")) {
+		} else if (colorGorro.equalsIgnoreCase("morado")) {
 			this.punteria += 2;
-		}
-		else {
+		} else {
 			this.punteria++;
 		}
-		
+
 	}
-	
-	
+
 }
